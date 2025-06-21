1ed187c75fbc1fd593ea6726855de96a4cf0e196
Celerity <celerity.rblx@gmail.com>
2025-06-21 14:03:30 +0300

    Add files via upload

---
+++ Tower Of Hell/Spawn Point.lua
@@ -0,0 +1,75 @@
+repeat
+    wait()
+until game:IsLoaded()
+wait(0.3)
+
+local ConsoleLogging = true
+
+local function Log(Text)
+    if ConsoleLogging then
+        warn(Text)
+    end
+end
+
+if getgenv().SpawnPoints == true then
+    Log("|Already Executed|")
+    return
+end
+
+getgenv().SpawnPoints = true
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local LocalPlayer = Players.LocalPlayer
+local PlaceId = {1962086868, 94971861814985, 3582763398}
+local CurrentSpawnPart = nil
+
+if table.find(PlaceId, game.PlaceId) then
+    Log("|Loading Spawn Points|")
+else
+    return
+end
+
+local function AddSpawnPoint(Platform)
+    Platform.Touched:Connect(function(Part)
+        if Part.Parent.Name == LocalPlayer.Name and CurrentSpawnPart ~= Platform then
+            if CurrentSpawnPart ~= nil then
+                if Platform.Position.Y > CurrentSpawnPart.Position.Y then
+                    CurrentSpawnPart = Platform
+                    Log("|New Spawn Point|")
+                end
+            else
+                CurrentSpawnPart = Platform
+                Log("|New Spawn Point|")
+            end
+        end
+    end)
+end
+
+local function Teleport(Character)
+    if CurrentSpawnPart ~= nil then
+        Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(CurrentSpawnPart.Position + Vector3.new(0, 2, 0))
+    end
+end
+
+for i, v in ipairs(workspace:GetDescendants()) do
+    if v.Name == "stop" or v.Name == "start" then
+        AddSpawnPoint(v)
+    end
+end
+
+workspace.DescendantAdded:Connect(function(Descendant)
+    RunService.Heartbeat:Wait()
+    if Descendant.Name == "stop" or Descendant.Name == "start" then
+        AddSpawnPoint(Descendant)
+    end
+end)
+
+LocalPlayer.CharacterAdded:Connect(function(Character)
+    local Ping = LocalPlayer:GetNetworkPing()
+    RunService.Heartbeat:Wait()
+    print(task.wait(Ping))
+    Teleport(Character)
+end)
+
+Log("|Spawn Points Loaded|")
